18f5d7c7f9befa7dc39ac1ea6a02892769c665a6
saimukesh <bethisaimukesh452@gmail.com>
2024-12-03 14:17:05 +0530

    sample.java

---
+++ sample.java
@@ -0,0 +1,11 @@
+public class MultiplyExample {
+   public static void main(String[] args) {
+       int a;
+       int b;
+       int c;
+       a = 5;
+       b = 58;
+       c = a*b; 
+       System.out.println("5*58 = " + c);
+   }
+}
